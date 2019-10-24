7ff4d9dcf078de40fa6eae84172408290cc5d42a
Sahil Bohot <sahil.may29@gmail.com>
2019-10-23 23:15:42 -0400

    Signed-off-by: Sahil Bohot <sahil.may29@gmail.com>

---
+++ Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -0,0 +1,56 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+ac = accuracy_score(y_test, y_pred)
+pt = precision_score(y_test, y_pred)
+rt = recall_score(y_test,y_pred)
+ft = f1_score(y_test, y_pred)
+print('Accuracy: ', ac, '\n','Precision: ', pt, '\n', 'Recall: ', rt,'\n', 'F1_score:', ft )
+
+#from sklearn.metrics import classification_report
+#target_names = ['class 0', 'class 1', 'class 2']
+#print(classification_report(y_test, y_pred))
